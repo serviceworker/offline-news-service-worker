c37a0b19e84ebb6b8d64847adc5aba1340ed51b4
Matthew Andrews <matt@mattandre.ws>
2014-09-08 19:36:38 +0100

    Attempt to use the caches polyfill instead

---
+++ public/caches-polyfill.js
@@ -0,0 +1,623 @@
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.caches=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var cacheDB = require('./cachedb');
+
+function castToRequest(request) {
+  if (!(request instanceof Request)) {
+    request = new Request(request);
+  }
+  return request;
+}
+
+function Cache() {
+  this._name = '';
+  this._origin = '';
+}
+
+var CacheProto = Cache.prototype;
+
+CacheProto.match = function(request, params) {
+  return cacheDB.match(this._origin, this._name, request, params);
+};
+
+CacheProto.matchAll = function(request, params) {
+  return cacheDB.matchAll(this._origin, this._name, request, params);
+};
+
+CacheProto.addAll = function(requests) {
+  requests = requests.map(castToRequest);
+
+  Promise.all(
+    requests.map(function(request) {
+      return fetch(request);
+    })
+  ).then(function(responses) {
+    return cacheDB.put(this._origin, this._name, responses.map(function(response, i) {
+      return [requests[i], response];
+    }));
+  }.bind(this));
+};
+
+CacheProto.add = function(request) {
+  return this.addAll([request]);
+};
+
+CacheProto.put = function(request, response) {
+  request = castToRequest(request);
+
+  if (!(response instanceof Response)) {
+    throw TypeError("Incorrect response type");
+  }
+
+  return cacheDB.put(this._origin, this._name, [[request, response]]);
+};
+
+CacheProto.delete = function(request, params) {
+  request = castToRequest(request);
+  return cacheDB.delete(this._origin, this._name, request, params);
+};
+
+CacheProto.keys = function(request, params) {
+  if (request) {
+    request = castToRequest(request);
+    return cacheDB.matchAllRequests(this._origin, this._name, request, params);
+  }
+  else {
+    return cacheDB.allRequests(this._origin, this._name);
+  }
+};
+
+module.exports = Cache;
+
+},{"./cachedb":2}],2:[function(require,module,exports){
+var IDBHelper = require('./idbhelper');
+
+function matchesVary(request, entryRequest, entryResponse) {
+  if (!entryResponse.headers.vary) {
+    return true;
+  }
+
+  var varyHeaders = entryResponse.headers.vary.split(',');
+  var varyHeader;
+
+  for (var i = 0; i < varyHeaders.length; i++) {
+    varyHeader = varyHeaders[i].trim();
+
+    if (varyHeader == '*') {
+      continue;
+    }
+
+    if (entryRequest.headers[varyHeader] != request.headers.get(varyHeader)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function createVaryID(entryRequest, entryResponse) {
+  var id = '';
+
+  if (!entryResponse.headers.vary) {
+    return id;
+  }
+
+  var varyHeaders = entryResponse.headers.vary.split(',');
+  var varyHeader;
+
+  for (var i = 0; i < varyHeaders.length; i++) {
+    varyHeader = varyHeaders[i].trim();
+
+    if (varyHeader == '*') {
+      continue;
+    }
+
+    id += varyHeader + ': ' + entryRequest.headers[varyHeader] + '\n';
+  }
+
+  return id;
+}
+
+function flattenHeaders(headers) {
+  var returnVal = {};
+  headers.forEach(function(val, key) {
+    returnVal[key] = val;
+  });
+
+  // so XHR can read the result (we don't have access to this header)
+  returnVal['Access-Control-Allow-Origin'] = location.origin;
+  return returnVal;
+}
+
+function entryToResponse(entry) {
+  var entryResponse = entry.response;
+  return new Response(entryResponse.body, {
+    status: entryResponse.status,
+    statusText: entryResponse.statusText,
+    headers: entryResponse.headers
+  });
+}
+
+function responseToEntry(response, body) {
+  return {
+    body: body,
+    status: response.status,
+    statusText: response.statusText,
+    headers: flattenHeaders(response.headers)
+  };
+}
+
+function entryToRequest(entry) {
+  var entryRequest = entry.request;
+  return new Request(entryRequest.url, {
+    mode: entryRequest.mode,
+    headers: entryRequest.headers,
+    credentials: entryRequest.headers
+  });
+}
+
+function requestToEntry(request) {
+  return {
+    url: request.url,
+    mode: request.mode,
+    credentials: request.credentials,
+    headers: flattenHeaders(request.headers)
+  };
+}
+
+function CacheDB() {
+  this.db = new IDBHelper('cache-polyfill', 1, function(db, oldVersion) {
+    switch (oldVersion) {
+      case 0:
+        var namesStore = db.createObjectStore('cacheNames', {
+          keyPath: ['origin', 'name']
+        });
+        namesStore.createIndex('origin', ['origin', 'added']);
+
+        var entryStore = db.createObjectStore('cacheEntries', {
+          keyPath: ['origin', 'cacheName', 'request.url', 'varyID']
+        });
+        entryStore.createIndex('origin-cacheName', ['origin', 'cacheName', 'added']);
+        entryStore.createIndex('origin-cacheName-urlNoSearch', ['origin', 'cacheName', 'requestUrlNoSearch', 'added']);
+        entryStore.createIndex('origin-cacheName-url', ['origin', 'cacheName', 'request.url', 'added']);
+    }
+  });
+}
+
+var CacheDBProto = CacheDB.prototype;
+
+CacheDBProto._eachCache = function(tx, origin, eachCallback, doneCallback, errorCallback) {
+  IDBHelper.iterate(
+    tx.objectStore('cacheNames').index('origin').openCursor(IDBKeyRange.bound([origin, 0], [origin, Infinity])),
+    eachCallback, doneCallback, errorCallback
+  );
+};
+
+CacheDBProto._eachMatch = function(tx, origin, cacheName, request, eachCallback, doneCallback, errorCallback, params) {
+  params = params || {};
+
+  var ignoreSearch = Boolean(params.ignoreSearch);
+  var ignoreMethod = Boolean(params.ignoreMethod);
+  var ignoreVary = Boolean(params.ignoreVary);
+  var prefixMatch = Boolean(params.prefixMatch);
+
+  if (!ignoreMethod &&
+      request.method !== 'GET' &&
+      request.method !== 'HEAD') {
+    // we only store GET responses at the moment, so no match
+    return Promise.resolve();
+  }
+
+  var cacheEntries = tx.objectStore('cacheEntries');
+  var range;
+  var index;
+  var indexName = 'origin-cacheName-url';
+  var urlToMatch = new URL(request.url);
+
+  urlToMatch.hash = '';
+
+  if (ignoreSearch) {
+    urlToMatch.search = '';
+    indexName += 'NoSearch';
+  }
+
+  // working around chrome bugs
+  urlToMatch = urlToMatch.href.replace(/(\?|#|\?#)$/, '');
+
+  index = cacheEntries.index(indexName);
+
+  if (prefixMatch) {
+    range = IDBKeyRange.bound([origin, cacheName, urlToMatch, 0], [origin, cacheName, urlToMatch + String.fromCharCode(65535), Infinity]);
+  }
+  else {
+    range = IDBKeyRange.bound([origin, cacheName, urlToMatch, 0], [origin, cacheName, urlToMatch, Infinity]);
+  }
+
+  IDBHelper.iterate(index.openCursor(range), function(cursor) {
+    var value = cursor.value;
+    
+    if (ignoreVary || matchesVary(request, cursor.value.request, cursor.value.response)) {
+      eachCallback(cursor);
+    }
+    else {
+      cursor.continue();
+    }
+  }, doneCallback, errorCallback);
+};
+
+CacheDBProto._hasCache = function(tx, origin, cacheName, doneCallback, errCallback) {
+  var store = tx.objectStore('cacheNames');
+  return IDBHelper.callbackify(store.get([origin, cacheName]), function(val) {
+    doneCallback(!!val);
+  }, errCallback);
+};
+
+CacheDBProto._delete = function(tx, origin, cacheName, request, doneCallback, errCallback, params) {
+  var returnVal = false;
+
+  this._eachMatch(tx, origin, cacheName, request, function(cursor) {
+    returnVal = true;
+    cursor.delete();
+  }, function() {
+    if (doneCallback) {
+      doneCallback(returnVal);
+    }
+  }, errCallback, params);
+};
+
+CacheDBProto.matchAllRequests = function(origin, cacheName, request, params) {
+  var matches = [];
+  return this.db.transaction('cacheEntries', function(tx) {
+    this._eachMatch(tx, origin, cacheName, request, function(cursor) {
+      matches.push(cursor.key);
+      cursor.continue();
+    }, undefined, undefined, params);
+  }.bind(this)).then(function() {
+    return matches.map(entryToRequest);
+  });
+};
+
+CacheDBProto.allRequests = function(origin, cacheName) {
+  var matches = [];
+
+  return this.db.transaction('cacheEntries', function(tx) {
+    var cacheEntries = tx.objectStore('cacheEntries');
+    var index = cacheEntries.index('origin-cacheName');
+
+    IDBHelper.iterate(index.openCursor(IDBKeyRange.bound([origin, cacheName, 0], [origin, cacheName, Infinity])), function(cursor) {
+      matches.push(cursor.key);
+      cursor.continue();
+    });
+  }).then(function() {
+    return matches.map(entryToRequest);
+  });
+};
+
+CacheDBProto.matchAll = function(origin, cacheName, request, params) {
+  var matches = [];
+  return this.db.transaction('cacheEntries', function(tx) {
+    this._eachMatch(tx, origin, cacheName, request, function(cursor) {
+      matches.push(cursor.value);
+      cursor.continue();
+    }, undefined, undefined, params);
+  }.bind(this)).then(function() {
+    return matches.map(entryToResponse);
+  });
+};
+
+CacheDBProto.match = function(origin, cacheName, request, params) {
+  var match;
+  return this.db.transaction('cacheEntries', function(tx) {
+    this._eachMatch(tx, origin, cacheName, request, function(cursor) {
+      match = cursor.value;
+    }, undefined, undefined, params);
+  }.bind(this)).then(function() {
+    return match ? entryToResponse(match) : undefined;
+  });
+};
+
+CacheDBProto.matchAcrossCaches = function(origin, request, params) {
+  var match;
+
+  return this.db.transaction(['cacheEntries', 'cacheNames'], function(tx) {
+    this._eachCache(tx, origin, function(cursor) {
+      var cacheName = cursor.value.name;
+
+      this._eachMatch(tx, origin, cacheName, request, function(cursor) {
+        match = cursor.value;
+        // we're done
+      }, undefined, undefined, params);
+
+      if (!match) { // continue if no match
+        cursor.continue();
+      }
+    }.bind(this));
+  }.bind(this)).then(function() {
+    return match ? entryToResponse(match) : undefined;
+  });
+};
+
+CacheDBProto.cacheNames = function(origin) {
+  var names = [];
+
+  return this.db.transaction('cacheNames', function(tx) {
+    this._eachCache(tx, origin, function(cursor) {
+      names.push(cursor.value.name);
+      cursor.continue();
+    }.bind(this));
+  }.bind(this)).then(function() {
+    return names;
+  });
+};
+
+CacheDBProto.delete = function(origin, cacheName, request, params) {
+  var returnVal;
+
+  return this.db.transaction('cacheEntries', function(tx) {
+    this._delete(tx, origin, cacheName, request, params, function(v) {
+      returnVal = v;
+    });
+  }.bind(this), {mode: 'readwrite'}).then(function() {
+    return returnVal;
+  });
+};
+
+CacheDBProto.createCache = function(origin, cacheName) {
+  return this.db.transaction('cacheNames', function(tx) {
+    var store = tx.objectStore('cacheNames');
+    store.add({
+      origin: origin,
+      name: cacheName,
+      added: Date.now()
+    });
+  }.bind(this), {mode: 'readwrite'});
+};
+
+CacheDBProto.hasCache = function(origin, cacheName) {
+  var returnVal;
+  return this.db.transaction('cacheNames', function(tx) {
+    this._hasCache(tx, origin, cacheName, function(val) {
+      returnVal = val;
+    });
+  }.bind(this)).then(function(val) {
+    return returnVal;
+  });
+};
+
+CacheDBProto.deleteCache = function(origin, cacheName) {
+  var returnVal = false;
+
+  return this.db.transaction(['cacheEntries', 'cacheNames'], function(tx) {
+    IDBHelper.iterate(
+      tx.objectStore('cacheNames').openCursor(IDBKeyRange.only([origin, cacheName])),
+      del
+    );
+
+    IDBHelper.iterate(
+      tx.objectStore('cacheEntries').index('origin-cacheName').openCursor(IDBKeyRange.bound([origin, cacheName, 0], [origin, cacheName, Infinity])),
+      del
+    );
+
+    function del(cursor) {
+      returnVal = true;
+      cursor.delete();
+      cursor.continue();
+    }
+  }.bind(this), {mode: 'readwrite'}).then(function() {
+    return returnVal;
+  });
+};
+
+CacheDBProto.put = function(origin, cacheName, items) {
+  // items is [[request, response], [request, response], …]
+  var item;
+
+  for (var i = 1; i < items.length; i++) {
+    if (items[i][0].method != 'GET') {
+      return Promise.reject(TypeError('Only GET requests are supported'));
+    }
+
+    // ensure each entry being put won't overwrite earlier entries being put
+    for (var j = 0; j < i; j++) {
+      if (items[i][0].url == items[j][0].url && matchesVary(items[j][0], items[i][0], items[i][1])) {
+        return Promise.reject(TypeError('Puts would overwrite eachother'));
+      }
+    }
+  }
+
+  return Promise.all(
+    items.map(function(item) {
+      // item[1].body.asBlob() is the old API
+      return item[1].asBlob ? item[1].asBlob() : item[1].body.asBlob();
+    })
+  ).then(function(responseBodies) {
+    return this.db.transaction(['cacheEntries', 'cacheNames'], function(tx) {
+      this._hasCache(tx, origin, cacheName, function(hasCache) {
+        if (!hasCache) {
+          throw Error("Cache of that name does not exist");
+        }
+
+        items.forEach(function(item, i) {
+          var request = item[0];
+          var response = item[1];
+          var requestEntry = requestToEntry(request);
+          var responseEntry = responseToEntry(response, responseBodies[i]);
+
+          var requestUrlNoSearch = new URL(request.url);
+          requestUrlNoSearch.search = '';
+          // working around Chrome bug
+          requestUrlNoSearch = requestUrlNoSearch.href.replace(/\?$/, '');
+
+          this._delete(tx, origin, cacheName, request, function() {
+            tx.objectStore('cacheEntries').add({
+              origin: origin,
+              cacheName: cacheName,
+              request: requestEntry,
+              response: responseEntry,
+              requestUrlNoSearch: requestUrlNoSearch,
+              varyID: createVaryID(requestEntry, responseEntry),
+              added: Date.now()
+            });
+          });
+
+        }.bind(this));
+      }.bind(this));
+    }.bind(this), {mode: 'readwrite'});
+  }.bind(this)).then(function() {
+    return undefined;
+  });
+};
+
+module.exports = new CacheDB();
+},{"./idbhelper":4}],3:[function(require,module,exports){
+var cacheDB = require('./cachedb');
+var Cache = require('./cache');
+
+function CacheStorage() {
+  this._origin = location.origin;
+}
+
+var CacheStorageProto = CacheStorage.prototype;
+
+CacheStorageProto._vendCache = function(name) {
+  var cache = new Cache();
+  cache._name = name;
+  cache._origin = this._origin;
+  return cache;
+};
+
+CacheStorageProto.match = function(request, params) {
+  return cacheDB.matchAcrossCaches(this._origin, request, params);
+};
+
+CacheStorageProto.get = function(name) {
+  return this.has(name).then(function(hasCache) {
+    var cache;
+    
+    if (hasCache) {
+      return this._vendCache(name);
+    }
+    else {
+      return null;
+    }
+  }.bind(this));
+};
+
+CacheStorageProto.has = function(name) {
+  return cacheDB.hasCache(this._origin, name);
+};
+
+CacheStorageProto.create = function(name) {
+  return cacheDB.createCache(this._origin, name).then(function() {
+    return this._vendCache(name);
+  }.bind(this), function() {
+    throw Error("Cache already exists");
+  });
+};
+
+CacheStorageProto.delete = function(name) {
+  return cacheDB.deleteCache(this._origin, name);
+};
+
+CacheStorageProto.keys = function() {
+  return cacheDB.cacheNames(this._origin);
+};
+
+module.exports = new CacheStorage();
+
+},{"./cache":1,"./cachedb":2}],4:[function(require,module,exports){
+function IDBHelper(name, version, upgradeCallback) {
+  var request = indexedDB.open(name, version);
+  this.ready = IDBHelper.promisify(request);
+  request.onupgradeneeded = function(event) {
+    upgradeCallback(request.result, event.oldVersion);
+  };
+}
+
+IDBHelper.supported = 'indexedDB' in self;
+
+IDBHelper.promisify = function(obj) {
+  return new Promise(function(resolve, reject) {
+    IDBHelper.callbackify(obj, resolve, reject);
+  });
+};
+
+IDBHelper.callbackify = function(obj, doneCallback, errCallback) {
+  function onsuccess(event) {
+    if (doneCallback) {
+      doneCallback(obj.result);
+    }
+    unlisten();
+  }
+  function onerror(event) {
+    if (errCallback) {
+      errCallback(obj.error);
+    }
+    unlisten();
+  }
+  function unlisten() {
+    obj.removeEventListener('complete', onsuccess);
+    obj.removeEventListener('success', onsuccess);
+    obj.removeEventListener('error', onerror);
+    obj.removeEventListener('abort', onerror);
+  }
+  obj.addEventListener('complete', onsuccess);
+  obj.addEventListener('success', onsuccess);
+  obj.addEventListener('error', onerror);
+  obj.addEventListener('abort', onerror);
+};
+
+IDBHelper.iterate = function(cursorRequest, eachCallback, doneCallback, errorCallback) {
+  var oldCursorContinue;
+
+  function cursorContinue() {
+    this._continuing = true;
+    return oldCursorContinue.call(this);
+  }
+
+  cursorRequest.onsuccess = function() {
+    var cursor = cursorRequest.result;
+
+    if (!cursor) {
+      if (doneCallback) {
+        doneCallback();
+      }
+      return;
+    }
+
+    if (cursor.continue != cursorContinue) {
+      oldCursorContinue = cursor.continue;
+      cursor.continue = cursorContinue;
+    }
+
+    eachCallback(cursor);
+
+    if (!cursor._continuing) {
+      if (doneCallback) {
+        doneCallback();
+      }
+    }
+  };
+
+  cursorRequest.onerror = function() {
+    if (errorCallback) {
+      errorCallback(cursorRequest.error);
+    }
+  };
+};
+
+var IDBHelperProto = IDBHelper.prototype;
+
+IDBHelperProto.transaction = function(stores, callback, opts) {
+  opts = opts || {};
+
+  return this.ready.then(function(db) {
+    var mode = opts.mode || 'readonly';
+
+    var tx = db.transaction(stores, mode);
+    callback(tx, db);
+    return IDBHelper.promisify(tx);
+  });
+};
+
+module.exports = IDBHelper;
+},{}]},{},[3])(3)
+});
